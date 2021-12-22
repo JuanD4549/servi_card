f382e07741f8d6e2b4b4688ca913ae1cc56a22b9
JuanD4549 <juandiego402@gmail.com>
2021-12-22 00:58:16 -0500

    Diseño card

--- lib/src/widgets/pedido_card.dart
+++ lib/src/widgets/pedido_card.dart
@@ -10,8 +10,17 @@ class PedidoCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return GFCard(
       boxFit: BoxFit.cover,
-      image: Image.asset(model.foto!.url!),
+      image: Image.asset(
+        "assets/img/imgTest.gif",
+        height: MediaQuery.of(context).size.height * 0.1,
+        width: MediaQuery.of(context).size.width,
+        fit: BoxFit.cover,
+      ),
+      showImage: true,
       title: GFListTile(
+        avatar: const GFAvatar(
+          backgroundImage: AssetImage('assets/img/3.png'),
+        ),
         title: Text(model.id! + " | " + model.hdr!),
         subTitle: Text(model.cliente!.nombre!),
       ),
@@ -23,13 +32,14 @@ class PedidoCard extends StatelessWidget {
               Navigator.push<void>(
                 context,
                 MaterialPageRoute<void>(
-                    builder: (BuildContext context) => PedidoPage(
-                          pedido: model,
-                        )),
+                  builder: (BuildContext context) => PedidoPage(
+                    pedido: model,
+                  ),
+                ),
               );
             },
             text: 'Detalles',
-          )
+          ),
         ],
       ),
     );
